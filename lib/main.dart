53e2a3e8866222ab451b495c4f2197604110f019
cenkvist <Brothars1@gmail.com>
2021-11-11 18:24:10 +0100

    uppgift 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,138 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(AttGora());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class AttGora extends StatelessWidget {
+  const AttGora({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MainView(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+//to do: dela upp & fixa stateful/interactive
+class MainView extends StatelessWidget {
+  const MainView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      appBar: _appbar(),
+      body: _lista(),
+      floatingActionButton: _fab(),
+    );
+  }
+
+  PreferredSizeWidget _appbar() {
+    return AppBar(
+        centerTitle: true,
+        title: const Text('To-Do List'),
+        backgroundColor: Colors.grey[900],
+        actions: const [
+          Icon(
+            Icons.menu_rounded,
+            color: Colors.white,
+            size: 24.0,
+          )
+        ]);
+  }
+
+//to do: gör om till containers - build(body:container(child(row(children[expand(child:container) etc etc
+  Widget _lista() {
+    return ListView(
+      children: [
+        Card(
+          child: ListTile(
+            title: Text('Vattna blommor',
+                style: TextStyle(decoration: TextDecoration.lineThrough)),
+            trailing: Wrap(
+              spacing: 36,
+              children: const [
+                Icon(Icons.check_box_rounded),
+                Icon(Icons.delete_forever_rounded),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Card(
+          child: ListTile(
+            title: Text('Rasta hund'),
+            trailing: Wrap(
+              spacing: 36,
+              children: const [
+                Icon(Icons.check_box_outline_blank_rounded),
+                Icon(Icons.delete_forever_rounded),
+              ],
+            ),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            title: Text('Vattna hund'),
+            trailing: Wrap(
+              spacing: 36,
+              children: const [
+                Icon(Icons.check_box_outline_blank_rounded),
+                Icon(Icons.delete_forever_rounded),
+              ],
+            ),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            title: Text('Göra färdigt listan'),
+            trailing: Wrap(
+              spacing: 36,
+              children: const [
+                Icon(Icons.check_box_outline_blank_rounded),
+                Icon(Icons.delete_forever_rounded),
+              ],
+            ),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            title: Text('Köpa hund',
+                style: TextStyle(decoration: TextDecoration.lineThrough)),
+            trailing: Wrap(
+              spacing: 36,
+              children: const [
+                Icon(Icons.check_box_rounded),
+                Icon(Icons.delete_forever_rounded),
+              ],
+            ),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            title: Text('Ge upp på plugget'),
+            trailing: Wrap(
+              spacing: 36,
+              children: const [
+                Icon(Icons.check_box_outline_blank_rounded),
+                Icon(Icons.delete_forever_rounded),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+//to do: inaktiv/null
+  Widget _fab() {
+    return FloatingActionButton(
+      onPressed: null,
+      child: Icon(Icons.add),
+      backgroundColor: Colors.grey[900],
+      foregroundColor: Colors.white,
     );
   }
 }
